7c71e4d7f90c0331d6cea56298c225689fb05c2a
moo1323@naver.com <moo1323@naver.com>
2020-08-27 01:17:42 +0900

    [feat] get data from gopax

--- src/components/CoinList.js
+++ src/components/CoinList.js
@@ -1,8 +1,26 @@
 import React from "react";
-import ProtoTypes from "prop-types";
 
-function CoinList() {
-  return <div className="coinlist">this is coinlist</div>;
+class CoinList extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getCoinList() {
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url = "https://api.gopax.co.kr/assets"; // site that doesn’t send Access-Control-*
+    fetch(proxyurl + url)
+      .then(response => response.json())
+      .then(contents => console.log(contents))
+      .catch(() =>
+        console.log("Can’t access " + url + " response. Blocked by browser?")
+      );
+  }
+  componentDidMount() {
+    this.getCoinList();
+  }
+  render() {
+    return <div className="coinlist">this is coinlist</div>;
+  }
 }
 
 export default CoinList;
