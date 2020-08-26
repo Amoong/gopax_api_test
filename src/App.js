a4702005f3f1ec5d60877b999a8cbe52fb8c9bf5
moo1323@naver.com <moo1323@naver.com>
2020-08-26 22:43:05 +0900

    basic setting

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>GOPAX API TEST</p>
+      </header>
+      <main className="App-main">this is main</main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/dist/App.test.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+var _react2 = require("@testing-library/react");
+
+var _App = _interopRequireDefault(require("./App"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
